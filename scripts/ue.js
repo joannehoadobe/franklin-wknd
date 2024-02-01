a88ce7b8565fb2e73991db26946ad3805c4fb6f9
Joanne Ho <joaho@adobe.com>
2024-02-01 16:02:20 -0700

    URGH

--- scripts/ue.js
+++ scripts/ue.js
@@ -1,10 +1,10 @@
 async function makeEditable() {
-    const aem = document.querySelector("meta[property='urn:adobe:aem:system:aemconnection']");  // was urn:adobe:aem:editor:aemconnection
+    const aem = document.querySelector("meta[property='urn:adobe:aue:system:aemconnection']");  // was urn:adobe:aem:editor:aemconnection
     if (aem) {
       const head = document.getElementsByTagName('head')[0];
   
       // Add URN Connection
-      aem.name = 'urn:adobe:aem:system:aemconnection';  // was urn:adobe:aem:editor:aemconnection
+      aem.name = 'urn:adobe:aue:system:aemconnection';  // was urn:adobe:aem:editor:aemconnection
       aem.content = 'aem:' + aem.content;
       aem.removeAttribute('property');
   
